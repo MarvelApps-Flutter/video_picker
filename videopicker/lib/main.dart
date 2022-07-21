bf380761dd5bdb7681e03871a5d8225be3dd860a
Ayush Aggarwal <ayush.aggarwal@successive.tech>
2022-07-21 08:23:07 +0530

    constants

--- videopicker/lib/main.dart
+++ videopicker/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'screens/homepage.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: HomePage());
+        home: SplashScreen());
   }
 }
